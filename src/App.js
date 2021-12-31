dedd42c65e39c5d97363ef4241b6cf650bd7dc50
DostyTheGreatKigali <abumucal89@gmail.com>
2021-12-31 21:59:10 +0000

    Axios DELETE made

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import PersonList from './components/PersonList.js';
 import PersonAdd from './components/PersonAdd';
+import PersonRemove from './components/PersonRemove';
 
 function App() {
   return (
     <div className="App">
       <PersonAdd/>
       <PersonList/>
+      <PersonRemove/>
     </div>
   );
 }
