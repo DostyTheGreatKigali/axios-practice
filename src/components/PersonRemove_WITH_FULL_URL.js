2fd90af5e8d1ab79ed03a7a298208e08376c65c3
DostyTheGreatKigali <abumucal89@gmail.com>
2021-12-31 22:03:02 +0000

    Added base instance of URL

--- src/components/PersonRemove_WITH_FULL_URL.js
+++ src/components/PersonRemove_WITH_FULL_URL.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import axios from 'axios';
-import API from '../api';
+import axios from 'axios';
 
 export default class PersonRemove extends React.Component {
   state = {
@@ -14,16 +13,11 @@ export default class PersonRemove extends React.Component {
   handleSubmit = event => {
     event.preventDefault();
 
-    // axios.delete(`https://jsonplaceholder.typicode.com/users/${this.state.id}`)
-    //   .then(res => {
-    //     console.log(res);
-    //     console.log(res.data);
-    //   })
-    API.delete(`users/${this.state.id}`)
-    .then(res => {
-      console.log(res);
-      console.log(res.data);
-    })
+    axios.delete(`https://jsonplaceholder.typicode.com/users/${this.state.id}`)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
   }
 
   render() {
